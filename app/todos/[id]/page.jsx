8e13450009c3590d3de2fec84c18a64a53b68d88
wahaj-hafeez <wahaj.hafeez@codedistrict.com>
2024-10-07 13:40:36 +0500

    implement the dynamic routes

---
+++ app/todos/[id]/page.jsx
@@ -0,0 +1,8 @@
+
+function page({params}) {
+  return (
+    <div>Params ID: {params.id}</div>
+  )
+}
+
+export default page
---
+++ app/todos/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function TodosPage() {
+  return (
+    <div>Todos Page</div>
+  )
+}
+
+export default TodosPage
