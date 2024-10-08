521100a5dc472f200ac5134ff56ba14729ef0fd8
wahaj-hafeez <wahaj.hafeez@codedistrict.com>
2024-10-08 13:40:09 +0500

    created a basic crud for todos

--- app/api/todos/route.js
+++ app/api/todos/route.js
@@ -1,11 +1,33 @@
-import { NextResponse } from "next/server"
+import { NextResponse } from "next/server";
 
-let todos =[
-    {id:1,name:"Get Groceries"},
-    {id:2,name:"Do Homework"},
-    {id:3,name:"Go to the Gym"}
-]
+let todos = [
+  { id: 1, name: "Get Groceries" },
+  { id: 2, name: "Do Homework" },
+  { id: 3, name: "Go to the Gym" },
+];
 
-export async function GET(){
-    return NextResponse.json({todos})
+export async function GET() {
+  return NextResponse.json({ todos });
 }
+
+export async function DELETE(request) {
+  const data = await request.json();
+  todos=todos.filter((todo)=>todo.id !== data.id);
+
+  console.log("debug", todos);
+  return NextResponse.json({todos});
+
+}
+
+export async function PUT(request)  {
+    const data = await request.json();
+    const updatedTodos = todos.map((todo)=>todo.id === data.id? data : todo);
+    return NextResponse.json({ updatedTodos });
+  }
+
+  export async function POST(request) {
+    const data = await request.json();
+    const newData = {id:todos.length + 1 , name:data.name}
+    todos.push(newData);
+    return NextResponse.json({ todos });
+  }
